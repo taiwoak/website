b6b7a847a6bd1e765bc62d01955126f766e32ade
Taiwo Akerele <taiwoakerele98@gmail.com>
2022-11-06 06:57:01 +0100

    build deployment files

---
+++ precache-manifest.08324c6b74cd96424cbf2d6c4d9709dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5ac17c1439cad163af3",
+    "url": "/website/static/css/main.2976af2f.chunk.css"
+  },
+  {
+    "revision": "a5ac17c1439cad163af3",
+    "url": "/website/static/js/main.a5ac17c1.chunk.js"
+  },
+  {
+    "revision": "1d16ec3dd438b8ac9a33",
+    "url": "/website/static/css/1.f9ad4e82.chunk.css"
+  },
+  {
+    "revision": "1d16ec3dd438b8ac9a33",
+    "url": "/website/static/js/1.1d16ec3d.chunk.js"
+  },
+  {
+    "revision": "df8eefd1f2dbc5f3d248",
+    "url": "/website/static/js/runtime~main.df8eefd1.js"
+  },
+  {
+    "revision": "8170889905e3d6b1fbda93664403a719",
+    "url": "/website/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/website/precache-manifest.ca0d57c069a10a6e3e075a2c6fb1b436.js"
+  "/website/precache-manifest.08324c6b74cd96424cbf2d6c4d9709dc.js"
 );
 
 workbox.clientsClaim();
